e88e5687921fbcc827b4e070aa312aa273cac618
jonamdessureault <91915461+jonamdessureault@users.noreply.github.com>
2022-01-06 13:55:40 -0500

    Add files via upload

---
+++ ProjetBingo-main/ProjetJeuPOO/Bingo/BingoBall.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//Classe représentant un objet boule 
+
+namespace ProjetJeuPOO.Bingo
+{
+    class BingoBall
+    {
+        
+        private char letter;
+        private int number;
+
+        public BingoBall(char letter, int number)
+        {
+            this.letter = letter;
+            this.number = number;
+        }
+
+
+
+
+    }
+}
---
+++ ProjetBingo-main/ProjetJeuPOO/Bingo/BingoCard.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Classe représentant un objet carte pour le joueur.
+// Un joueur a au minimum une carte.
+
+namespace ProjetJeuPOO.Bingo
+{
+    class BingoCard: IBingoBoulier
+    {
+        int[,] carteJoueur = new int[5, 5];
+        int[,] carteAnnonceur = new int[5, 15];
+
+        public BingoCard()
+        {
+
+        }
+
+        public void add(BingoBall element)
+        {
+            //Chaque fois qu'une boule est pigée au hasard, on ajoute ce résultat à carteAnnonceur
+        }
+
+        public BingoBall getRanbomBall()
+        {
+            //Choisi une boule au hasard parmis la liste listeBoules
+            return null;
+        }
+
+        public int getSize()
+        {
+           //Nous informe sur le contenu des cartes
+           return carteAnnonceur.Length;
+        }
+
+        public bool isEmpty()
+        {
+            //Nous informe si oui ou non les cartes contiennent quelque chose
+            return (true);
+        }
+
+        public void restartBoulier()
+        {
+            //Réinitialise le contenu de carteAnnonceur
+        }
+
+        public void CreerCartes()
+        {
+
+        }
+
+        public void AfficherCartes()
+        {
+
+        }
+
+        public void AfficherCarteA()
+        {
+
+        }
+    }
+}
